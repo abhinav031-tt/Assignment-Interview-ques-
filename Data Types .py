d9b4c3b15210209394378f9327af98257543144d
abhinav031-tt <113112279+abhinav031-tt@users.noreply.github.com>
2022-11-04 23:06:20 +0530

    Create Data Types .py

---
+++ Data Types .py
@@ -0,0 +1,93 @@
+     # 20 Question of Data Types 
+
+# Q1. What are data types in programming?     
+
+''' Ans : Data types are the classification of data that tells the compiler or interpreter
+     how the programmer intends to use that data.'''
+
+     # Types of Data types :
+     #1: integers
+     #2: floating point
+     #3: bool
+     #4: strings
+
+
+
+# Q2. What is the difference between a primitive and non-primitive data type?
+
+''' Ans: a) Primitive Data type:A primitive data type is one that is built into the language and is not derived from any other type.Ex-int, float, and char
+       b): Non Primitive Data type:A non-primitive data type is one that is not built into the language and is instead derived from a primitive data type.Ex-An Array.
+
+'''
+
+#Q5. Is it possible to store multiple values in a single variable? If yes, then how?
+'''  Ans: Yes, it is possible to store multiple values in a single variable in a few different ways. One way is to store the values as a list or array. 
+        Another way is to store the values as a string, with each value separated by a delimiter.
+'''
+
+#Q8. What is the main difference between immutable and mutable objects?
+''' Ans:Immutable objects are those that cannot be changed after they are created. Mutable objects, 
+        on the other hand, can be changed after they are created.
+'''
+
+
+#Q9. Why are these data types known as Python’s core data types?
+''' Ans:They are known as the core data types because they are part of the Python language itself and are always available to create other objects, you usually need to call functions in imported modules.'''
+
+#Q10. What is a dictionary in Python?
+''' Ans: Python dictionary is one of the supported data types in Python. It is an unordered collection of elements. The elements in dictionaries are stored as key-value pairs. Dictionaries are indexed by keys
+'''
+#Q11. What are functions in Python?
+''' Ans: function is a block of code which is executed only when a call is made to the function'''
+
+#Q12. What are the common built-in data types in Python?
+'''Ans:Python supports the below-mentioned built-in data types:
+
+a):Immutable data types:
+Number
+String
+Tuple
+b):Mutable data types:
+List
+Dictionary
+set '''
+
+#Q13.What are local variables and global variables in Python?
+'''Ans:Local variable: Any variable declared inside a function is known as Local variable and it’s accessibility remains inside that function only.
+
+       Global Variable: Any variable declared outside the function is known as Global variable and it can be easily accessible by any function present throughout the program.'''
+
+#Q14.What is type conversion in Python?
+'''Ans:Python provides you with a much-needed functionality of converting one form of data type into the needed one and this is known as type conversion.
+ Types:
+  Implicit Type Conversion
+  Explicit Type Conversion
+
+'''
+
+#Q15.Is python case sensitive?
+'''Ans:Yes, Python is a case sensitive language. This means that Function and function both are different in pythons like SQL and Pascal.'''
+
+#Q16. What are Python packages?
+'''Ans:A Python package refers to the collection of different sub-packages and modules based on the similarities of the function.'''
+
+#Q17.Why are these data types known as Python’s core data types?
+'''Ans:They are known as the core data types because they are part of the Python language itself and are always available to create other objects, you usually need to call functions in imported modules.'''
+
+#Q18.What are Data Types?
+'''Ans:A data type is a set of values and a set of operations defined on data. An implementation of a data type is an expression of data and operations in terms of a specific programming language such as Java, C ++, or Python.'''
+
+#Q19.What is a dynamically typed language?
+''' Ans:What is a dynamically type typed language?
+Dynamically-typed languages are those where the interpreter assigns variables a type at runtime based on the variable's value at the time.
+'''
+
+#Q20. Do we need to declare variables with data types in Python?
+'''Ans:No. Python is a dynamically typed language, I.E., Python Interpreter automatically identifies the data type of a variable based on the type of value assigned to the variable.'''
+
+
+
+
+
+
+
