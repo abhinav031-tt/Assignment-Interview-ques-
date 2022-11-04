b62fb7f3bdc7cbde6a8b4c13281eefdf35b63937
abhinav031-tt <113112279+abhinav031-tt@users.noreply.github.com>
2022-11-04 23:08:48 +0530

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,124 @@
+# 20 Question of Operators
+
+#Q1. What is the output of print(2%6)
+'''Ans: The ouput is 2   '''
+
+
+#Q2. What is the output of print(2 * 3 ** 3 * 4)
+'''Ans:  216                              '''
+
+#Q3.What is the output of the following code
+   #print(bool(0), bool(3.14159), bool(-3), bool(1.0+1j))
+''' Ans: False,True,True,True
+'''
+
+#Q3.What is the output of the following code
+   # x = 100 , y = 50
+   # print(x and y)
+'''Ans: 50
+'''
+
+#Q4. What is the value of the following Python Expression
+      #print(36 / 4)
+'''Ans: The output is 9.0
+'''
+
+#Q5: 4 is 100 in binary and 11 is 1011. What is the output of the following bitwise operators?
+#a = 4 , b = 11
+#print(a | b)
+# print(a >> 2)
+'''Ans: The Output is 15 and 1
+'''
+#Q6: what is the output?
+#x = 6
+#y = 2
+#print(x ** y)
+#print(x // y)
+
+''' Ans: The output is 36 , 3
+'''
+#Q7.What is meant by an Operator in Python?
+'''Ans:These are the special symbols in python and are used to execute an Arithmetic or Logical computation. An operator alone cannot perform an activity, it needs an Operand.'''
+
+#Q8. What is meant by an Operand in Python?
+'''Ans: An Operand is a value that the operator needs to complete a task.
+'''
+
+#Q9.Types of operators in Python?
+'''Ans: Arithmetic operators
+Comparison operators
+Assignment operators
+Logical operators
+Membership operators
+Special operators
+Identity operators
+'''
+#Q10.x = 20
+#y = 25
+# Output: x == y is False
+#print('x == y is',x==y)
+'''Ans: x == y is False
+'''
+
+#Q11.What is pep 8?
+'''Ans: PEP stands for Python Enhancement Proposal. It is a set of rules that specify how to format Python code for maximum readability.
+'''
+#Q12.What is the another name of Comparison Operators?
+'''Ans:Relational Operators  '''
+
+#Q13.Why do we use ” < > “operator?
+'''Ans:It checks if the value of two operands are equal or not, if values are not equal then condition becomes true'''
+
+
+#Q14. What statement is used in Python if the statement is required syntactically, but no action is required for the program?
+
+'''Ans: Pass statement   '''
+
+#Q15.In the Python statement x = a + 5 - b:
+
+#a and b are ________
+#a + 5 - b is ________
+'''Ans:operands, an expression
+'''
+
+#Q16.Should you use the == operator to determine whether objects of type float are equal?
+'''Ans: No, not agood idea       '''
+
+
+#Q17:a = 100
+  #b = 200
+#What is the value of the expression a and b?
+'''Ans : 100
+'''
+
+
+#Q18.msg = "life is beautiful"
+#if msg == "I love you":
+ #   print ("propose")
+#else:
+  #  print ("wait xP")
+'''Ans: wait xP      '''
+
+#Q19.a = 10+20
+#b = 100 - 1
+#c = 50 / 7
+#d = 50 // 7
+#e = 10 % 8
+#f = 5 ** 2
+#print (a,b,c,d,e,f, sep="\n ")
+'''Ans.30
+99
+7.142857142857143
+7
+4
+25
+'''
+#Q20.lst = ["soccer", "swimming", "running", "skiing"]
+   # if "rock climbing" not in lst:
+    #    print ("boo")
+'''Ans:['boo'] '''
+
+
+
+
+   
